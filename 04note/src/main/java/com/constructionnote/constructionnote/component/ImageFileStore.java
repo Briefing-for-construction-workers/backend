68896ca2dba4bade2678e7e27a9918010850303e
Arin Kim <mwtgal20@gmail.com>
2024-04-14 14:13:29 +0900

    :sparkles: ADD : GET User Profile

--- 04note/src/main/java/com/constructionnote/constructionnote/component/ImageFileStore.java
+++ 04note/src/main/java/com/constructionnote/constructionnote/component/ImageFileStore.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.UUID;
 
 @Component
@@ -31,5 +30,36 @@ public class ImageFileStore {
         String ext = originalFilename.substring(idx);
 
         return uuid + ext;
+    }
+
+    public byte[] getFile(String filePath) throws Exception {
+        FileInputStream inputStream = null;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        try {
+            inputStream = new FileInputStream(fileDirPath + filePath);
+        }
+        catch (FileNotFoundException e) {
+            throw new Exception("Can't find file");
+        }
+
+        int readCount = 0;
+        byte[] buffer = new byte[1024];
+        byte[] fileArray = null;
+
+        try {
+            while((readCount = inputStream.read(buffer)) != -1){
+                outputStream.write(buffer, 0, readCount);
+            }
+            fileArray = outputStream.toByteArray();
+            inputStream.close();
+            outputStream.close();
+
+        }
+        catch (IOException e) {
+            throw new Exception("Can't convert file");
+        }
+
+        return fileArray;
     }
 }
