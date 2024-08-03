fc66bc4149d836c71ebe620cda61e100274f7a89
Arin Kim <mwtgal20@gmail.com>
2024-08-03 14:59:46 +0900

    :fire: remove : #24 구인구직 게시판 분리로 불필요 기능 삭제

--- 04note/src/main/java/com/constructionnote/constructionnote/repository/PostRepository.java
+++ 04note/src/main/java/com/constructionnote/constructionnote/repository/PostRepository.java
@@ -3,5 +3,5 @@ package com.constructionnote.constructionnote.repository;
 import com.constructionnote.constructionnote.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
